f79fd2e7ac3395302410a14e117bad865680ec28
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-23 21:36:42 +0100

    UE07_py_argparse: add program logic for CVcrack

--- UE07_py_argparse/cvcrack.py
+++ UE07_py_argparse/cvcrack.py
@@ -1,6 +1,11 @@
 __author__ = "Karun Sandhu"
 
 import argparse
+import os
+import sys
+
+from UE05_kasiski.kasiski import Kasiski
+from UE05_kasiski.caesar import Caesar
 
 
 class CVcrackNamespace(argparse.Namespace):
@@ -37,5 +42,37 @@ def parse_arguments() -> CVcrackNamespace:
     return parser.parse_args(namespace=CVcrackNamespace())
 
 
+def process_file(args: CVcrackNamespace) -> None:
+    """
+    Processes the input file by cracking a file encrypted with the Vigenere or
+    the Caesar cipher.
+
+    :param args: Parsed command-line arguments containing file paths and cipher settings.
+    """
+    if not os.path.isfile(args.infile):
+        print(f"{args.infile}: No such file or directory", file=sys.stderr)
+        sys.exit(1)
+
+    with open(args.infile, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    if args.cipher in ["caesar", "c"]:
+        cipher_name = "Caesar"
+        cipher = Caesar()
+        key = cipher.crack(text, 1)[0]
+    else:
+        cipher_name = "Vigenere"
+        cipher = Kasiski(text)
+        key = cipher.crack_key(6)
+
+    if args.verbose:
+        print(
+            f"Cracking {cipher_name}-encrypted file {args.infile}: key = {key}"
+        )
+    else:
+        print(key)
+
+
 if __name__ == "__main__":
     args = parse_arguments()
+    process_file(args)
