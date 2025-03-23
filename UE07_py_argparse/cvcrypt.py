ff791c1dfacaac6474c5dc11725fe6f9913371b6
karun <129101708+MrSom3body@users.noreply.github.com>
2025-03-23 20:23:18 +0100

    UE07_py_argparse: add program logic for CVcrypt

--- UE07_py_argparse/cvcrypt.py
+++ UE07_py_argparse/cvcrypt.py
@@ -1,6 +1,10 @@
 __author__ = "Karun Sandhu"
 
 import argparse
+import sys
+from UE05_kasiski.caesar import Caesar
+from UE05_kasiski.vigenere import Vigenere
+import os
 
 
 class CVcryptNamespace(argparse.Namespace):
@@ -54,3 +58,43 @@ def parse_arguments() -> CVcryptNamespace:
         parser.error("One of --encrypt or --decrypt must be specified.")
 
     return args
+
+
+def process_file(args: CVcryptNamespace) -> None:
+    """
+    Processes the input file by encrypting or decrypting it and writes the
+    result to the output file.
+
+    :param args: Parsed command-line arguments containing file paths and cipher settings.
+    """
+    if not os.path.isfile(args.infile):
+        print(f"{args.infile}: No such file or directory", file=sys.stderr)
+        sys.exit(1)
+
+    with open(args.infile, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    if args.cipher in ["caesar", "c"]:
+        cipher = Caesar(args.key)
+    else:
+        cipher = Vigenere(args.key)
+
+    if args.encrypt:
+        result = cipher.encrypt(text)
+        action = "Encrypting"
+    else:
+        result = cipher.decrypt(text)
+        action = "Decrypting"
+
+    with open(args.outfile, "w") as f:
+        _ = f.write(result)
+
+    if args.verbose:
+        print(
+            f"{action} {args.cipher} with key = {args.key} from file {args.infile} into file {args.outfile}"
+        )
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    process_file(args)
