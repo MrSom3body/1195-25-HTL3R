ae3aaaba1e23845605954329cf21a8f593f28893
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-05-29 14:08:08 +0200

    treewide: switch to uv from poetry

--- flake.nix
+++ flake.nix
@@ -1,87 +1,94 @@
 {
-  inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    nixpkgs-python.url = "github:cachix/nixpkgs-python";
-  };
+  description = "Hello world flake using uv2nix";
 
-  nixConfig = {
-    extra-trusted-public-keys = [
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-    ];
-    extra-substituters = [
-      "https://devenv.cachix.org"
-      "https://nixpkgs-python.cachix.org"
-    ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    devenv,
-    systems,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
     ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  }: let
+    inherit (nixpkgs) lib;
+
+    workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel";
+    };
+
+    # Extend generated overlay with build fixups
+    #
+    # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
+    # This is an additional overlay implementing build fixups.
+    # See:
+    # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+    pyprojectOverrides = _final: _prev: {
+      # Implement build fixups here.
+      # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+      # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+    };
+
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    python = pkgs.python313;
+
+    pythonSet =
+      (pkgs.callPackage pyproject-nix.build.packages {
+        inherit python;
+      }).overrideScope
+      (
+        lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+          pyprojectOverrides
+        ]
+      );
   in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-      devenv-test = self.devShells.${system}.default.config.test;
-    });
+    packages.x86_64-linux.default = pythonSet.mkVirtualEnv "1195-25-htl3r-env" workspace.deps.default;
 
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              packages = with pkgs; [
-                git # duh
-              ];
-
-              languages = {
-                python = {
-                  enable = true;
-                  version = "3.13";
-                  poetry = {
-                    enable = true;
-                    activate.enable = true;
-                    install.enable = true;
-                  };
-                  libraries = [
-                    pkgs.stdenv.cc.cc
-                    pkgs.zlib
-                  ];
-                };
-
-                java = {
-                  enable = true;
-                };
-              };
-
-              enterShell =
-                # bash
-                ''
-                  tput setaf 2; tput bold; echo "git version"; tput sgr0
-                  git --version
-                  tput setaf 2; tput bold; echo "python version"; tput sgr0
-                  python --version
-                  tput setaf 2; tput bold; echo "java version"; tput sgr0
-                  java --version
-                '';
-
-              pre-commit.hooks = {
-                autoflake.enable = true;
-                ruff.enable = true;
-              };
-            }
-          ];
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        python
+        pkgs.uv
+      ];
+      env =
+        {
+          # Prevent uv from managing Python downloads
+          UV_PYTHON_DOWNLOADS = "never";
+          # Force uv to use nixpkgs Python interpreter
+          UV_PYTHON = python.interpreter;
+        }
+        // lib.optionalAttrs pkgs.stdenv.isLinux {
+          # Python libraries often load native shared objects using dlopen(3).
+          # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+          LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
         };
-      });
+      shellHook = ''
+        unset PYTHONPATH
+      '';
+    };
   };
 }
